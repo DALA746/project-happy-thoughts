dca35322872539189dd1cc2b9069d4969ea83fad
DALA746 <darya.lapata@gmail.com>
2023-02-15 10:12:19 +0100

    added new url

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,8 +1,8 @@
 export const API_URL =
-  'https://project-happy-thoughts-api-363is6kojq-uc.a.run.app/thoughts';
+  'https://project-happy-thoughts-api-production.up.railway.app/thoughts';
 
 export const LIKES_URL = (thoughtId) =>
-  `https://project-happy-thoughts-api-363is6kojq-uc.a.run.app/thoughts/${thoughtId}/like`;
+  `https://project-happy-thoughts-api-production.up.railway.app/thoughts/${thoughtId}/like`;
 
 export const DELETE_URL = (thoughtId) =>
-  `https://project-happy-thoughts-api-363is6kojq-uc.a.run.app/thoughts/${thoughtId}`;
+  `https://project-happy-thoughts-api-production.up.railway.app/thoughts/${thoughtId}`;
